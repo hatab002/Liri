e58f2e94013146c7c06c2f8103d380561062dc75
hatab002 <hatab002@umn.edu>
2018-04-19 09:36:36 -0500

    Initial setup

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var request = require('request');
+var omdb = require()
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var command = procces.argv(2)
+
+function spotifyCall(){
+
+}
