da72bb4c68cf2046d761598d42751aef0d844e40
hatab002 <hatab002@umn.edu>
2018-04-20 00:54:37 -0500

    added omdb request

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
 var keys = require('./keys.js')
-// var omdb = require()
+var omdb = require('omdb')
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 let command = process.argv[2]
@@ -16,7 +16,12 @@ let spotifyCall = function(){
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-        else{console.log(data.tracks.items[1])}
+        else{
+            console.log(`Album: ${data.tracks.items[1].album.name}`)
+            console.log(`Artist: ${data.tracks.items[1].artists[0].name}`)
+            console.log(`Preview URL: ${data.tracks.items[1].preview_url}`)
+            console.log(`Song name: ${data.tracks.items[1].name}`)
+        }
       });
 };
 
@@ -25,8 +30,8 @@ let twitter = function(){
     client.get('statuses/user_timeline', handle, function(error, tweets, response) {
         if (!error && response.statusCode === 200){
             for (i = 0; i < tweets.length; i++){
+                console.log(tweets[i].user.name);
                 console.log(tweets[i].created_at);
-                console.log(tweets[i].user.name)
                 console.log(tweets[i].text);
                 
             }
@@ -35,6 +40,27 @@ let twitter = function(){
 
 }
 
+let movieTime = function(){
+
+    request(`http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+    console.log("The movie's title: " + JSON.parse(body).Title);
+    console.log("The movie's release date: " + JSON.parse(body).Released);
+    console.log("The movie's actors: " + JSON.parse(body).Actors);
+    console.log("The movie's plot: " + JSON.parse(body).Plot);
+    console.log("The movie's language: " + JSON.parse(body).Language);
+
+    console.log("The movie's imdb rating: " + JSON.parse(body).imdbRating);
+    console.log("The movie's rotten tomatoe rating: " + JSON.parse(body).Ratings[1].Value);
+  
+  }
+});
+}
+if (command === "movie" && search === ""){
+    let search = "Mr. Nobody";
+    movieTime();
+    }
 if (command === "spotify"){
 
     spotifyCall();
@@ -46,4 +72,9 @@ if (command === "twitter"){
     twitter();
 } else {
     console.log("twitter not working")
+}
+if (command === "movie"){
+    movieTime();
+} else {
+    console.log("movie not working")
 }
