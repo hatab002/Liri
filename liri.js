a8c22cf724e9f979f08e3279d1a5b29ce4bb0771
hatab002 <hatab002@umn.edu>
2018-04-19 20:58:31 -0500

    Initial spotify and twitter functions working

--- liri.js
+++ liri.js
@@ -1,12 +1,49 @@
+
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
-var omdb = require()
+var keys = require('./keys.js')
+// var omdb = require()
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var command = procces.argv(2)
+let command = process.argv[2]
+let search = process.argv[3]
+let handle = {screen_name: 'notsodomtorett'}
 
-function spotifyCall(){
+let spotifyCall = function(){
+    spotify.search({ type: 'track', query: search }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        else{console.log(data.tracks.items[1])}
+      });
+};
 
+let twitter = function(){
+   
+    client.get('statuses/user_timeline', handle, function(error, tweets, response) {
+        if (!error && response.statusCode === 200){
+            for (i = 0; i < tweets.length; i++){
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].user.name)
+                console.log(tweets[i].text);
+                
+            }
+        } else {console.log(error)}
+     });
+
+}
+
+if (command === "spotify"){
+
+    spotifyCall();
+} else{
+    console.log("spotify not working")
+};
+
+if (command === "twitter"){
+    twitter();
+} else {
+    console.log("twitter not working")
 }
