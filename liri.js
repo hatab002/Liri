1a9e23a88a1e242d3e5dca5ab7652e3ec384df3a
hatab002 <hatab002@umn.edu>
2018-04-23 14:42:41 -0500

    Added inquirer

--- liri.js
+++ liri.js
@@ -9,26 +9,34 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 let command = process.argv[2];
-let search = process.argv[3];
+let userSearch = process.argv[3];
+for (let i = 4; i < process.argv.length; i++) {
+    userSearch += `+${process.argv[i]}`;
+}
 const twitterParam = {
     screen_name: 'notsodomtorett',
     count: 20
 };
-const spotifyParam = {
-    type: 'track',
-    query: search
-}
-
-let spotifyCall = function (search) {
-    spotify.search(spotifyParam, function (err, data) {
+let spotifyCall = function () {
+    if (userSearch === undefined) {
+        userSearch = "track:The+Sign+artist:Ace+of+Base";
+    }
+    spotify.search({
+        type: 'track',
+        query: userSearch
+    }, function (err, data) {
         let songInfo = data.tracks.items[0];
         if (err) {
             return console.log('Error occurred: ' + err);
         } else {
             console.log(`Album: ${songInfo.album.name}`)
             console.log(`Artist: ${songInfo.artists[0].name}`)
-            console.log(`Preview URL: ${songInfo.preview_url}`)
             console.log(`Song name: ${songInfo.name}`)
+            if (songInfo.preview_url) {
+                console.log(`Preview URL: ${songInfo.preview_url}`)
+            } else {
+                console.log("Preview URL: Sorry, there is no preview URL.")
+            }
         }
     });
 };
@@ -48,10 +56,10 @@ let twitter = function () {
     });
 }
 
-let movieTime = function (search) {
-    var queryURL = `http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`;
-    request(queryURL, function (err, response, body) {
-        if (!err && response.statusCode === 200) {
+let movieTime = function (userSearch) {
+    var queryURL = `http://www.omdbapi.com/?t=${userSearch}&y=&plot=short&apikey=trilogy`;
+    request(queryURL, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             let parsedBody = JSON.parse(body)
             console.log("The movie's title: " + parsedBody.Title);
             console.log("The movie's release date: " + parsedBody.Released);
@@ -61,63 +69,73 @@ let movieTime = function (search) {
             console.log("The movie's imdb rating: " + parsedBody.imdbRating);
             console.log("The movie's rotten tomatoes rating: " + parsedBody.Ratings[1].Value);
         } else {
-            console.log(err)
+            console.log("Error: Movie not found!");
         }
     });
 }
 
 let justDoItAlready = function () {
     fs.readFile("random.txt", "utf8", function (err, data) {
-        var queryText = data.split(", ")
-        spotifyCall(queryText[1])
+        queryText = data.split(",")
+        userSearch = queryText[1]
+        spotifyCall(userSearch);
     })
 };
-
-switch (command) {
-    case "my-tweets":
-        twitter();
-        break;
-    case "spotify-this-song":
-        spotifyCall();
-        break;
-    case "movie-this":
-        if (search) {
-            movieTime(search);
-        } else {
-            movieTime("Mr. Nobody")
-        }
-        break;
-    case "do-what-it-says":
-        justDoItAlready();
-}
-
-
-// inquirer.prompt([{
-//     type: "list",
-//     name: "yourWishIsMyCommand",
-//     message: "Hello there! How may I be of service?",
-//     choices: ["Do what you will!", "Show me my tweets!", "Tell me about this movie!", "Spotify this song for me!"]
-// }, ]).then((response) => {
-//     if (response.yourWishIsMyCommand === "Do what you will!") {
-//         justDoItAlready();
-//     } else if (response.yourWishIsMyCommand === "Show me my tweets!") {
+// switch (command) {
+//     case "my-tweets":
 //         twitter();
-//     } else if (response.yourWishIsMyCommand === "Tell me about this movie!") {
-//         inquirer.prompt([{
-//             type: "input",
-//             name: "movie",
-//             message: "Which movie would you like me to lookup?"
-//         }, ]).then((responseMovie) => {
-//             movieTime(responseMovie.movie);
-//         });
-    
-//     } else if (response.yourWishIsMyCommand === "Spotify this song for me!") {
-//         inquirer.prompt([{
-//             type: "input",
-//             name: "song",
-//             message: "Enter a song name"
-//         }, ]).then((songResponse) => {
-//             spotifyCall(songResponse.song);
-//         });
-//     }
-// });
+//         break;
+//     case "spotify-this-song":
+//         spotifyCall();
+//         break;
+//     case "movie-this":
+//         if (userSearch) {
+//             movieTime(userSearch);
+//         } else {
+//             movieTime("Mr. Nobody")
+//         }
+//         break;
+//     case "do-what-it-says":
+//         justDoItAlready();
+//         break;
+// }
+
+function inq() {
+    inquirer.prompt([{
+        type: "rawlist",
+        name: "yourWishIsMyCommand",
+        message: "It's you again! How may I be of service?",
+        choices: ['Do what you will!', 'Show me my tweets!', 'Tell me about this movie!', 'Spotify this song for me!']
+    }]).then(function (response) {
+        choiceResponse = response.yourWishIsMyCommand;
+
+        switch (choiceResponse) {
+            case 'Do what you will!':
+                justDoItAlready();
+                break;
+            case 'Show me my tweets!':
+                twitter();
+                break;
+            case 'Tell me about this movie!':
+                inquirer.prompt([{
+                    type: "input",
+                    name: "movie",
+                    message: "Which movie would you like me to lookup?"
+                }, ]).then(function (responseOmdb) {
+                    movieTime(responseOmdb.movie);
+                });
+                break;
+            case 'Spotify this song for me!':
+                inquirer.prompt([{
+                    type: "input",
+                    name: "song",
+                    message: "Enter a song name"
+                }, ]).then(function (responseSpotify) {
+                    userSearch = responseSpotify.song
+                    spotifyCall(userSearch);
+                });
+                break;
+        }
+    });
+}
+inq();
